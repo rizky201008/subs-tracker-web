916dda63ad0e73dd53f96a560391549fab0028ab
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-25 10:33:40 +0700

    refactor: component = modify SubscriptionList component

--- app/components/SubscriptionList.tsx
+++ app/components/SubscriptionList.tsx
@@ -1,10 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Loading } from "./Loading";
+
 export default function SubscriptionList() {
-  return (
-    <div className="card bg-[#EFDB91] w-full max-w-4xl shadow-lg">
+  const [list, setList] = useState<number[]>([]);
+  const [loading, setLoading] = useState(false);
+  const getSubscriptions = async () => {
+    setLoading(true);
+    for (let index = 0; index < 5; index++) {
+      setList((prev) => [...prev, index]);
+    }
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    getSubscriptions();
+  }, []);
+
+  if (loading) {
+    return <Loading size={"md"} type={0} />;
+  }
+  return list.map((item, key) => (
+    <div className="card bg-[#EFDB91] w-full max-w-4xl shadow-lg" key={key}>
       <div className="card-body">
         <h2 className="card-title">Netflix</h2>
         <p>100 USD {"(Monthly)"}</p>
       </div>
     </div>
-  );
+  ));
 }
