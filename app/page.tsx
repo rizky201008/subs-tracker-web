92aaf1456c1637375443cba73343b2571a169e22
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-25 10:35:40 +0700

    refactor: page = modify main page, add container

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
+import { Container } from "./components/Container";
 import SubscriptionList from "./components/SubscriptionList";
 import SubscriptionSummary from "./components/SubscriptionSummary";
 import TopBar from "./components/TopBar";
 
 export default function Home() {
-  const listData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <div className="flex flex-col w-full px-6 md:px-12 py-6 items-center flex-grow-0 gap-3">
+    <Container>
       <TopBar />
       <SubscriptionSummary />
       <h2 className="w-full text-start text-lg font-bold max-w-4xl">
         All subscriptions
       </h2>
-      {listData && listData.map((item, key) => <SubscriptionList key={key} />)}
-    </div>
+        <SubscriptionList />
+    </Container>
   );
 }
